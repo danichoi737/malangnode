8b7c76b842d73b9973552ab713a2146c0f8d470e
danichoi737 <danichoi737@gmail.com>
2023-11-20 23:33:39 +0900

    Add basic HTTP server

---
+++ server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express from "express";
+// Routers
+import router from "./routes/index.js";
+
+// dotenv
+dotenv.config();
+
+// Express
+const HTTP_PORT = "HTTP_PORT";
+const app = express();
+app.set(HTTP_PORT, process.env.HTTP_PORT || 8000);
+app.set("view engine", "ejs");
+app.set("views", "views");
+app.use(express.static("public"));
+app.use(router);
+
+app.listen(app.get(HTTP_PORT), () => {
+  console.log("[HTTP] Server listening: " + app.get(HTTP_PORT));
+});
