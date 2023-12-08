958a5c3fa78d52175c113a9d4ace354538096fb0
danichoi737 <danichoi737@gmail.com>
2023-12-08 23:26:23 +0900

    Add auth router and sign in page

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/signin", (req, res) => {
+  res.render("signin");
+});
+
+export default router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import "dotenv/config.js";
 import express from "express";
 // Routers
-import router from "./routes/index.js";
+import authRouter from "./routes/auth.js";
+import pageRouter from "./routes/index.js";
 // Modules
 import websocket from "./src/websocket.js";
 
@@ -12,7 +13,9 @@ app.set(HTTP_PORT, process.env.HTTP_PORT || 8000);
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
-app.use(router);
+
+app.use(authRouter);
+app.use(pageRouter);
 
 const httpServer = app.listen(app.get(HTTP_PORT), () => {
   console.log("[HTTP] Server listening: " + app.get(HTTP_PORT));
